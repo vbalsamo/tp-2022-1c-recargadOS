cba30a9166837fb2192f6902e291f5ae4f7a116b
valenhz <vbalsamo@frba.utn.edu.ar>
2022-04-20 20:05:21 -0300

    agrego logger del kernel

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -16,14 +16,27 @@ void deserializarSegun(t_paquete* paquete, int socket){
 	close(socket);
 }
 
+t_log* iniciar_logger(void)
+{
+    t_log* nuevo_logger;
+    if((nuevo_logger = log_create("kernellog.log", "TP0", 1, LOG_LEVEL_INFO)) == NULL){
+        printf("No se pudo crear el log\n");
+        exit(1);
+    }
+    return nuevo_logger;
+}
+
 int main(int argc, char* argv[]) {  
-    t_log * nuevolog = log_create();
+    
     validarParametros(argc, argv);
     t_config * config = config_create(argv[1]);
     char * IP_KERNEL = config_get_string_value(config, "IP_KERNEL");
     char * PUERTO_ESCUCHA = config_get_string_value(config, "PUERTO_ESCUCHA");
    
     int socket = iniciar_servidor(IP_KERNEL, PUERTO_ESCUCHA);
+
+    t_log * nuevolog = iniciar_logger();
+    log_info(nuevolog, "soy un log");
 
     while(1){
         int socket_cliente = esperar_cliente(socket);
