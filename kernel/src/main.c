b05ea402de8ae85ebfdffeee79bfc36521f16d4f
valenhz <balvalentino@gmail.com>
2022-05-12 17:45:21 -0300

    creamos conexión entre kernel y memoria
    Co-authored-by: tbavutti <tbavutti@users.noreply.github.com>

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,13 +1,14 @@
 #include <main.h>
 
+t_proceso * nuevo_proceso;
+
 void deserializarSegun(t_paquete* paquete, int socket){
 
 	switch(paquete->codigo_operacion){
 		case PROCESO:{
             t_proceso * proceso = deserializarProceso(paquete->buffer->stream);
-            iniciarPcb(proceso);
-            //t_log * nuevolog = log_create("kernel.log", "kernel", 1, LOG_LEVEL_INFO);
-            free(proceso);
+            nuevo_proceso = proceso;
+            //free(proceso);
 			break;
         }
 
@@ -26,24 +27,36 @@ void deserializarSegun(t_paquete* paquete, int socket){
 	close(socket);
 }
 
+
+
 int main(int argc, char* argv[]) {  
-    
+    nuevo_proceso = malloc(sizeof(t_proceso));
+
     validarParametros(argc, argv);
     t_config * config = config_create(argv[1]);
     char * IP_KERNEL = config_get_string_value(config, "IP_KERNEL");
     char * PUERTO_ESCUCHA = config_get_string_value(config, "PUERTO_ESCUCHA");
 
+    char * IP_MEMORIA = config_get_string_value(config, "IP_MEMORIA"); 
+    char * PUERTO_MEMORIA = config_get_string_value(config,"PUERTO_MEMORIA");
+
+    int socket_cliente = crear_conexion(IP_MEMORIA, PUERTO_MEMORIA);
+
     int socket = iniciar_servidor(IP_KERNEL, PUERTO_ESCUCHA); 
 
     inicializarEstados();
+
+    //pruebita
+    
+
 
     while(1){
         int socket_cliente = esperar_cliente(socket);
         t_paquete * paquete = recibirPaquete(socket_cliente);
         deserializarSegun(paquete, socket_cliente);
-        break;
+        iniciarPcb(nuevo_proceso);
+        free(nuevo_proceso);
     }
-
     
     return 0;
 }
