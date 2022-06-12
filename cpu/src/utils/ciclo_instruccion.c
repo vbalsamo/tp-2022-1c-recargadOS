67b82dc8bf84713e5ac698dfa753a6c9b8d748a0
Juan Salazar <juanfrancisco14sc@gmail.com>
2022-06-12 18:52:19 -0300

    Avance TLB
    Co-authored-by: Clara Belén Busca <belenbusca@users.noreply.github.com>
    Co-authored-by: EstebanDer <EstebanDer@users.noreply.github.com>
    Co-authored-by: Valentino Balsamo <balvalentino@gmail.com>

--- cpu/src/utils/ciclo_instruccion.c
+++ cpu/src/utils/ciclo_instruccion.c
@@ -108,7 +108,6 @@ void * execute_read(uint32_t direccion_logica){
     return dato;
 }
 
-
 void execute_write(uint32_t direccion_logica, void * dato){
     uint32_t direccionFisica = consultarDireccionFisica(tablaPaginasPrimerNivelPCB, direccion_logica);
     memoria_write(direccionFisica, dato);
